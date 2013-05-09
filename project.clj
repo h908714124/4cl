bcb806c5cbfa00c66e2d4902f2140c63aa59b1bb
methylene <theairspeedindicator@yahoo.com>
2013-05-09 15:45:08 -0400

    clojure hello world

--- project.clj
+++ project.clj
@@ -10,10 +10,7 @@
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.elasticsearch/elasticsearch-river-wikipedia
-                  "1.2.0-SNAPSHOT"]
-                 [org.twitter4j/twitter4j-stream "3.0.3"]
                  [slingshot "0.10.3"]]
   :plugins [[lein-bin "0.3.2"]]
-  :main stream2es.main
+  :main d.main
   :bin {:bootclasspath true})
